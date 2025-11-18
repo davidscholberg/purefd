0662ff6033b45e6b6a8c31c54814ba6fc7c1e443
David Scholberg <recombinant.vector@gmail.com>
2025-11-18 01:04:45 -0500

    compile regex once instead of on each search (duhhhh)

--- src/ListDir.hs
+++ src/ListDir.hs
@@ -9,21 +9,30 @@ import qualified Data.ByteString.Char8 as BSC
 import DirStream
 import Foreign.Ptr
 import Text.Regex.TDFA
+import Text.Regex.TDFA.ByteString
+
+compileRegex :: String -> IO (Maybe Regex)
+compileRegex regexStr = do
+  let eitherRegex = compile defaultCompOpt defaultExecOpt $ BSC.pack regexStr
+  case eitherRegex of
+    Left e -> fail e
+    Right r -> pure $ Just r
 
 listDir :: Maybe String -> String -> IO ()
-listDir maybeRegex pathStr = do
+listDir maybeRegexStr pathStr = do
   let path = BSC.pack pathStr
   pathIsDir <- BS.useAsCString path isDir
   unless
     pathIsDir
     (fail $ "path is not a directory: " ++ pathStr)
+  maybeRegex <- maybe (pure Nothing) compileRegex maybeRegexStr
   BS.useAsCString
     path
     ( \pathCStr ->
         withDirStream pathCStr (processDirent maybeRegex path)
     )
 
-processDirent :: Maybe String -> BS.ByteString -> Ptr CDir -> IO ()
+processDirent :: Maybe Regex -> BS.ByteString -> Ptr CDir -> IO ()
 processDirent maybeRegex basePath dirStream = do
   maybeDirEntry <- readDirent dirStream
   case maybeDirEntry of
@@ -32,7 +41,7 @@ processDirent maybeRegex basePath dirStream = do
       dirEntry <- BS.packCString dirEntryCStr
       let path = basePath <> BS.singleton 47 <> dirEntry
       when
-        (maybe True (dirEntry =~) maybeRegex)
+        (maybe True (`matchTest` dirEntry) maybeRegex)
         (BS.putStr $ path <> BS.singleton 10)
       BS.useAsCString
         path
